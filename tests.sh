f8ecc740f2c4aff82ab8d8a77e8980d4f551e289
Ezekiel Chopper <echopper@petrode.com>
2016-05-11 13:35:55 -0600

    trying to clean up the repo layout

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for testfile in test_*.py; do
-    py.test -v --junitxml results_${testfile:5:-3}.xml $testfile
+for testfile in tests/test_*.py; do
+    py.test -v --junitxml results_${testfile:11:-3}.xml $testfile
 done
 exit 0
