656d464c6e3a4c342501af907fe332740fd3203f
Ezekiel Chopper <echopper@petrode.com>
2016-05-26 15:32:43 -0600

    updating test runner to discover tests again

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for testfile in tests/test_*.py; do
-    py.test -v --junitxml results_${testfile:11:-3}.xml $testfile
+for testfile in control/tests/test_*.py; do
+    py.test -v --junitxml results_${testfile:19:-3}.xml $testfile
 done
 exit 0
