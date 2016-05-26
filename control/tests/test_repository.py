9174fa3cbbbb4c94d07ad18084c4042c8ffd5774
Ezekiel Chopper <echopper@petrode.com>
2016-05-26 15:03:53 -0600

    repository tests are runnable now

--- control/tests/test_repository.py
+++ control/tests/test_repository.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-from .context import control
+from control.repository import Repository
 
 
 class RepositoryTest(unittest.TestCase):
@@ -11,7 +11,7 @@ class RepositoryTest(unittest.TestCase):
 
     def test_creation(self):
         """Ensure that minimal options produce a correct Repository object"""
-        repository = control.Repository('ubuntu')
+        repository = Repository('ubuntu')
         self.assertTrue(repository.repo == 'ubuntu:latest')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.get_pull_image_name() == 'ubuntu')
@@ -22,7 +22,7 @@ class RepositoryTest(unittest.TestCase):
 
     def test_tagging(self):
         """Make sure that tagging an image produces the correct result"""
-        repository = control.Repository('ubuntu', '14.04')
+        repository = Repository('ubuntu', '14.04')
         self.assertTrue(repository.repo == 'ubuntu:14.04')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.get_pull_image_name() == 'ubuntu')
@@ -33,7 +33,7 @@ class RepositoryTest(unittest.TestCase):
 
     def test_registry(self):
         """Test a registry running on port 443"""
-        repository = control.Repository('ubuntu', '14.04', 'docker.petrode.com')
+        repository = Repository('ubuntu', '14.04', 'docker.petrode.com')
         self.assertTrue(repository.repo == 'docker.petrode.com/ubuntu:14.04')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.get_pull_image_name() == 'docker.petrode.com/ubuntu')
@@ -44,7 +44,7 @@ class RepositoryTest(unittest.TestCase):
 
     def test_registry_and_port(self):
         """Test a registry running on port 443"""
-        repository = control.Repository('ubuntu', '14.04', 'docker.petrode.com', '5002')
+        repository = Repository('ubuntu', '14.04', 'docker.petrode.com', '5002')
         self.assertTrue(repository.repo == 'docker.petrode.com:5002/ubuntu:14.04')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.get_pull_image_name() == 'docker.petrode.com:5002/ubuntu')
@@ -59,7 +59,7 @@ class MatcherTest(unittest.TestCase):
 
     def test_matching_only_latest_image(self):
         """Create a repo based on just an image name"""
-        repository = control.Repository.match('ubuntu')
+        repository = Repository.match('ubuntu')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.tag == 'latest')
         self.assertTrue(repository.domain is None)
@@ -67,7 +67,7 @@ class MatcherTest(unittest.TestCase):
 
     def test_matching_image(self):
         """Create a repo based on an image name with a tag"""
-        repository = control.Repository.match('ubuntu:14.04')
+        repository = Repository.match('ubuntu:14.04')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.tag == '14.04')
         self.assertTrue(repository.domain is None)
@@ -75,7 +75,7 @@ class MatcherTest(unittest.TestCase):
 
     def test_matching_image_with_domain(self):
         """Create a repo based on an image from a registry"""
-        repository = control.Repository.match('docker.petrode.com/ubuntu:14.04')
+        repository = Repository.match('docker.petrode.com/ubuntu:14.04')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.tag == '14.04')
         self.assertTrue(repository.domain == 'docker.petrode.com')
@@ -83,7 +83,7 @@ class MatcherTest(unittest.TestCase):
 
     def test_match_repo_with_registry(self):
         """Create a repo based on an image from a registry running on not 443"""
-        repository = control.Repository.match('docker.petrode.com:5002/ubuntu:14.04')
+        repository = Repository.match('docker.petrode.com:5002/ubuntu:14.04')
         self.assertTrue(repository.image == 'ubuntu')
         self.assertTrue(repository.tag == '14.04')
         self.assertTrue(repository.domain == 'docker.petrode.com')
